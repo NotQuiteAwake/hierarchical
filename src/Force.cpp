a40aacea4b7f7e17a8aea88fdc10b0d22d3489fc
Jimmy C <11169901+NotQuiteAwake@users.noreply.github.com>
2024-03-20 00:29:45 +0800

    add self-interaction assert in Force
    Force is now safeguarded with asserts that prevent calculating
    self-interaction (which gives nan). This happened in FMM due to an
    incorrect loop and was not discovered for quite some time.

--- src/Force.cpp
+++ src/Force.cpp
@@ -1,17 +1,41 @@
-#include "Force.hpp"
 #include <cmath>
+#include <cassert>
+#include "Force.hpp"
 
 namespace sim {
 
+Vec Force::GetForce(const Particle& p1, const Particle& p2) const {
+    if (CheckDistinct(p1, p2)) {
+        return ForceLaw(p1, p2);
+    } else {
+        // in debug version CheckDistinct trips asserts.
+        // in production as last ditch effort don't return nan's...
+        return Vec();
+    }
+}
+
+bool Force::CheckDistinct(const Particle& p1, const Particle& p2) const {
+    bool same_par_addr = (&p1 == &p2);
+    bool same_pos = (p1.pos == p2.pos);
+    assert(!same_par_addr);
+    assert(!same_pos);
+    return !(same_par_addr || same_pos);
+}
+
 Gravity::Gravity(double G): mG(G) {};
 
-Vec Gravity::GetForce(const Particle& p1, const Particle& p2) const {
+Vec Gravity::ForceLaw(const Particle& p1, const Particle& p2) const {
     Vec dr = p2.pos - p1.pos;
     double dist = dr.GetNorm();
+
+    // no 'self-interaction'!
+    assert(&p1 != &p2);
+    assert(dist);
+
     return  dr * mG * p1.GetMass() * p2.GetMass() / pow(dist, 3);
 } 
 
-Vec DummyForce::GetForce(const Particle& p1, const Particle& p2) const {
+Vec DummyForce::ForceLaw(const Particle& p1, const Particle& p2) const {
     return Vec();
 }
 
--- src/Force.hpp
+++ src/Force.hpp
@@ -13,7 +13,14 @@ class Force {
         virtual ~Force() = default;
         // We can only do pairwise interactions...
         // Further require that forces obey Newton III.
-        virtual Vec GetForce(const Particle& p1, const Particle& p2) const = 0;
+        // employ template method pattern.
+        Vec GetForce(const Particle& p1, const Particle& p2) const;
+
+    protected:
+        virtual Vec ForceLaw(const Particle& p1, const Particle& p2) const = 0;
+
+    private:
+        bool CheckDistinct(const Particle& p1, const Particle& p2) const;
 };
 
 class Gravity : public Force {
@@ -22,12 +29,12 @@ class Gravity : public Force {
 
     public:
         Gravity(double G = 1);
-        Vec GetForce(const Particle& p1, const Particle& p2) const override;
+        Vec ForceLaw(const Particle& p1, const Particle& p2) const override;
 };
 
 class DummyForce : public Force {
     public:
-        Vec GetForce(const Particle& p1, const Particle& p2) const override;
+        Vec ForceLaw(const Particle& p1, const Particle& p2) const override;
 };
 
 }
